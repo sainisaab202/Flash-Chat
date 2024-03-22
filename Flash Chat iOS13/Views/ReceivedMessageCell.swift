f07d980b31a15b3a2397b4d758219ada86c13f5e
Gurpreet Saini <sainisaab202@gmail.com>
2024-03-22 16:32:59 -0400

    added new custom cell, fixed location for received vs send messages

---
+++ Flash Chat iOS13/Views/ReceivedMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceivedMessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by GurPreet SaiNi on 2024-03-22.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ReceivedMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
